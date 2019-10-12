5ad2c225e1fe9a41bd8a3d591c9436e7d9f3bbce
Rutvik Patel <rutvik48@gmail.com>
2019-10-12 11:54:21 -0700

    Working on SignUp Screen

--- BookSwap/Controller/SignUpScreen.swift
+++ BookSwap/Controller/SignUpScreen.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class SignUpScreen: UIViewController {
 
+    //Labels and TextFields from Main.Storyboard
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,5 +32,49 @@ class SignUpScreen: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+        
+        if (checkIfEmpty(userNameTextField) && checkIfEmpty(emailTextField) && checkIfEmpty(passwordTextField) && checkIfEmpty(confirmPasswordTextField)){}
+        
+        print(checkPassword(passwordTextField.text!, confirmPasswordTextField.text!))
+    }
+    
+    func checkIfTextFieldIsEmpty() -> Bool {
+        
+        checkIfEmpty(userNameTextField)
+        checkIfEmpty(emailTextField)
+        checkIfEmpty(passwordTextField)
+        checkIfEmpty(confirmPasswordTextField)
+        
+        return true
+    }
+    
+    func checkIfEmpty(_ textField: UITextField) -> Bool{
+        
+        if textField.text!.isEmpty {
+            //change color of the text field to red to inform user
+            textField.backgroundColor = UIColor.red
+            return false
+            
+        }else{
+            
+            textField.backgroundColor = UIColor.white
+            return true
+            
+        }
+    }
+    
+    
+    func checkPassword(_ pswd1: String,_ pswd2: String ) -> Bool{
+    
+        if (pswd1 == pswd2 && !(pswd1.isEmpty)){
+            return true
+        }
+        
+        return false
+        
+    }
+    
 
 }

6afc3c7f816c4af8f4b41fb93c855d52377014a6
Asliddin Asliev <you@example.com>
2019-10-12 14:01:52 -0400

    edit sign up screen, add sign up class

---
+++ BookSwap/Controller/SignUpScreen.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpScreen.swift
+//  BookSwap
+//
+//  Created by Asliddin Asliev on 10/12/19.
+//  Copyright © 2019 RV. All rights reserved.
+//
+
+import UIKit
+
+class SignUpScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

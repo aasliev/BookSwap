058b1607e38a3e7768efd108a42e7b9029fa57c0
Asliddin Asliev <you@example.com>
2019-12-01 00:18:03 -0500

    Added tab bar

---
+++ BookSwap/Controller/TabBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarViewController.swift
+//  BookSwap
+//
+//  Created by Asliddin Asliev on 11/30/19.
+//  Copyright © 2019 RV. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //self.navigationItem.title =
+    }
+    
+
+}

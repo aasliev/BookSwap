5676ae5461c82c234f5457c234f00e41594e76ba
Rutvik Patel <rutvik48@gmail.com>
2019-12-04 18:42:06 -0800

    Bug of duplicating books in OwnedBook scree has been fixed. Nedded to empty the array called 'otherOwnedBook.' It was adding up the results everytime table reloads the data, added otherOwnedBook.removeAll() next to where resetting the core data entity called 'OthersOwnedBook.' WishList has been implemented too for other user. Functionalities such as move to OwnedBook and Delete has been desabled for other user. Next need to work on showing the search result in both OwnedBook and WishList page.

--- BookSwap/Controller/ViewControllers/Books/WishListScreen.swift
+++ BookSwap/Controller/ViewControllers/Books/WishListScreen.swift
@@ -12,7 +12,103 @@ import SwipeCellKit
 
 class WishListScreen: UITableViewController {
 
+    //Array which takes objects of WishList
     var itemArray = [WishList]()
+    var otherWishList = [OthersWishList]()
+    
+    //context of Core Data file
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    //Instances of other classes, which will be used to access the methods
+    let databaseIstance = FirebaseDatabase.shared
+    let authInstance = FirebaseAuth.sharedFirebaseAuth
+    let coreDataClassInstance = CoreDataClass.sharedCoreData
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadItems()
+        tableView.rowHeight = 80
+        tableView.refreshControl = refresher
+    }
+    
+    
+    //MARK: TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return authInstance.isOtherUserEmpty() ?  itemArray.count : otherWishList.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "wishCell", for: indexPath) as! WishListTableViewCell
+        
+        if authInstance.isOtherUserEmpty(){
+            
+            cell.nameOfTheBook?.text = itemArray[indexPath.row].bookName
+            cell.authorOfTheBook?.text = itemArray[indexPath.row].author
+            
+        } else {
+            
+            cell.nameOfTheBook?.text = otherWishList[indexPath.row].bookName
+            cell.authorOfTheBook?.text = otherWishList[indexPath.row].author
+        }
+        
+        cell.delegate = self
+        return cell
+    }
+    
+    
+    //MARK: - Model Manipulation Methods
+    @objc func loadItems(with request: NSFetchRequest<WishList> = WishList.fetchRequest()) {
+        do {
+            
+            //checks which user is currently on the WishList page
+            //NOTE: Other User will be true if user open someone else's WishList
+            if authInstance.isOtherUserEmpty() {
+                
+                itemArray = try context.fetch(request)
+            } else {
+                
+                databaseIstance.getListOfOwnedBookOrWishList(usersEmail: authInstance.otherUser, trueForOwnedBookFalseForWishList: false) { (dataDictionary) in
+                    
+                    self.loadDataForOtherUser(dict: dataDictionary)
+                }
+            }
+            
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
+    
+    
+    //Loads the data inside OthersWishList array, which is received from Firestore
+    func loadDataForOtherUser(dict : Dictionary<Int  , Dictionary<String  , Any>>) {
+        
+        //Clearing the data stored inside Core Data file
+        self.coreDataClassInstance.resetOneEntitie(entityName: "OthersWishList")
+        
+        //Clearing the array which holds objects of 'OthersOwnedBook'
+        self.otherWishList.removeAll()
+        
+        for (_, data) in dict {
+            
+            let newWhishListBook = OthersWishList(context: self.context)
+            newWhishListBook.bookName = (data[self.databaseIstance.BOOKNAME_FIELD] as! String)
+            newWhishListBook.author = (data[self.databaseIstance.AUTHOR_FIELD] as! String)
+            
+            self.otherWishList.append(newWhishListBook)
+        }
+        
+        self.coreDataClassInstance.saveContext()
+        
+        print("This is DICT: ", dict as AnyObject)
+        self.tableView.reloadData()
+        
+    }
+    
+    
     lazy var refresher: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.tintColor = UIColor.white
@@ -20,19 +116,6 @@ class WishListScreen: UITableViewController {
         
         return refreshControl
     }()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    let databaseIstance = FirebaseDatabase.shared
-    let authInstance = FirebaseAuth.sharedFirebaseAuth
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        loadItems()
-        tableView.rowHeight = 80
-        tableView.refreshControl = refresher
-    }
     
     
     @objc func refreshItems(){
@@ -44,43 +127,15 @@ class WishListScreen: UITableViewController {
         self.tableView.reloadData()
         
     }
-
-    
-    //MARK: TableView DataSource Methods
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "wishCell", for: indexPath) as! WishListTableViewCell
-        cell.nameOfTheBook?.text = itemArray[indexPath.row].bookName
-        cell.authorOfTheBook?.text = itemArray[indexPath.row].author
-        
-        cell.delegate = self
-        return cell
-    }
-    
-    
-    
-    
-    //MARK: - Model Manipulation Methods
-    @objc func loadItems(with request: NSFetchRequest<WishList> = WishList.fetchRequest()) {
-        do {
-            itemArray = try context.fetch(request)
-        } catch {
-            print("Error fetching data from context \(error)")
-        }
-    }
 }
-
-
 
 
 //MARK: Search
 
-extension WishListScreen: UISearchBarDelegate
-{
+extension WishListScreen: UISearchBarDelegate {
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
         let request : NSFetchRequest<WishList> = WishList.fetchRequest()
         request.predicate = NSPredicate(format: "bookName CONTAINS[cd] %@", searchBar.text!)
         
@@ -105,9 +160,13 @@ extension WishListScreen: UISearchBarDelegate
 }
 
 //MARK: SwipeCellKit
-
 extension WishListScreen: SwipeTableViewCellDelegate{
+    
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+   
+        //Checks if user is on someone else's WishList page. If yes, disable the feature of swiping left to delete and right for moving from WishList to OwnedBook..
+        if (!authInstance.isOtherUserEmpty()) { return nil }
+        
         guard orientation == .right else {
             let moreAction = SwipeAction(style: .default, title: "Move") { action, indexPath in
                 //move book from wishList to Owned books
@@ -133,8 +192,7 @@ extension WishListScreen: SwipeTableViewCellDelegate{
             
             return [moreAction]
             
-            
-            }
+        }
         
         let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
             
